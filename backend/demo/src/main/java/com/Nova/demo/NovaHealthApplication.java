f9e960c003aa0f33a51d8030932c0e280e4cd880
Apurba Mukherjee <2005apurba@gmail.com>
2026-01-28 18:35:43 +0530

    backend edited

---
+++ backend/demo/src/main/java/com/Nova/demo/NovaHealthApplication.java
@@ -0,0 +1,13 @@
+package com.Nova.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaHealthApplication.class, args);
+	}
+
+}
---
+++ backend/demo/src/test/java/com/Nova/demo/NovaHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nova.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
